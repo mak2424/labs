2137d02b0d4920f81aded339f1fe12b53b4a7f21
Fedor <dreeborn@mail.ru>
2019-06-04 16:00:39 +0300

    z2 lab2

--- mat_app/lab2/lab2.py
+++ mat_app/lab2/lab2.py
@@ -4,6 +4,7 @@ import random
 import statistics
 import math
 
+
 def save(name='', fmt='png'):
     pwd = os.getcwd()
     iPath = './pictures/'
@@ -12,6 +13,7 @@ def save(name='', fmt='png'):
     os.chdir(iPath)
     plt.savefig('{}.{}'.format(name, fmt), fmt='png')
     os.chdir(pwd)
+
 
 def z1():
     print(f"""
@@ -104,4 +106,100 @@ def z1():
     plt.hist([data1["box_select"], data2["box_select"], data3["box_select"]])
     save("lab2-1(box)", fmt='png')
     plt.show()
+
+
+def z2():
+    events = [0.337, 0.375, 0.209, 0.079]
+    allias = [-1, -1, -1, -1]
+    parts = events.copy()
+
+    def init_alias():
+        init_var = sum(events)/len(events)
+        for var in range(len(events) - 1):
+            max_value = max(events)
+            max_index = events.index(max_value)
+            min_value = min(events)
+            min_index = events.index(min_value)
+
+
+            diff = init_var - min_value
+            #print(f"{max_value} {min_value} {diff}")
+            events[min_index] = events[min_index] + diff
+            allias[min_index] = max_index
+            events[max_index] = events[max_index] - diff
+            parts[max_index] = parts[max_index] - diff
+
+    def generate_value():
+        indx = random.randint(0, 3)
+        u_value = random.random()
+        if u_value > parts[indx]:
+            if allias[indx] != -1:
+                return allias[indx]
+            else:
+                return indx
+        else:
+            return indx
+
+    def exp(n):
+        init_alias()
+        experiment = []
+        select_blood = []
+        for i in range(n):
+            people1 = generate_value() + 1  # донор
+            select_blood.append(str(people1))
+            people2 = generate_value() + 1  # больной
+            select_blood.append(str(people2))
+
+            if people2 == 4:
+                experiment.append(1)
+            elif people2 == 3 and (people1 == 1 or people1 == 3):
+                experiment.append(1)
+            elif people2 == 2 and (people1 == 1 or people1 == 2):
+                experiment.append(1)
+            elif people2 == 1 and people1 == 1:
+                experiment.append(1)
+            else:
+                experiment.append(0)
+
+        Mx = experiment.count(1) / len(experiment)
+
+        D = 0
+        for x in experiment:
+            D += (x - Mx)
+        D = D / len(experiment)
+
+        print(f"Exp {n}: M={Mx} D={D}")
+
+        return select_blood
+
+    data1 = exp(100)
+    data2 = exp(500)
+    data3 = exp(1000)
+
+    fig1 = plt.figure()
+    plt.title('Задание 2: 100')
+    plt.ylabel('Количество событий')
+    plt.xlabel('Группа крови')
+    plt.hist([data1])
+    save("lab2-2(100)", fmt='png')
+    plt.show()
+
+    fig1 = plt.figure()
+    plt.title('Задание 2: 500')
+    plt.ylabel('Количество событий')
+    plt.xlabel('Группа крови')
+    plt.hist([data2])
+    save("lab2-2(500)", fmt='png')
+    plt.show()
+
+    fig1 = plt.figure()
+    plt.title('Задание 2: 1000')
+    plt.ylabel('Количество событий')
+    plt.xlabel('Группа крови')
+    plt.hist([data3])
+    save("lab2-2(1000)", fmt='png')
+    plt.show()
+
+
 z1()
+z2()
