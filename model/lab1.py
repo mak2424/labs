7f8b5d251911eb700a094a28d09fc3e75466e2d6
Fedor <dreeborn@mail.ru>
2019-06-04 18:03:48 +0300

    model lab 1

--- model/lab1.py
+++ model/lab1.py
@@ -6,26 +6,38 @@ J = -1 #  тип взаимодействия, определяющий осно
 
 def init_matrix(matrix_length=MATRIX_LENGTH):
     new_matrix = []
-    for width in range(0, matrix_length):
+    values = [1, -1]
+    for height in range(0, matrix_length):
         inner_matrix = []
-        for height in range(0, matrix_length):
-            inner_matrix.append(random.randint(0, 1))
+        for width in range(0, matrix_length):
+            inner_matrix.append(random.choice(values))
         new_matrix.append(inner_matrix)
 
     print_matrix(new_matrix)
     return new_matrix
 
+
 def print_matrix(matrix):
     string = ""
-    for width in range(0, len(matrix)):
-        for height in range(0, len(matrix[0])):
-            string += f"{matrix[width][height]} "
+    for height in range(0, len(matrix)):
+        for width in range(0, len(matrix[0])):
+            string += f"{width}{height} {matrix[width][height]}\t"
         string += "\n"
 
     print(string)
 
-def calculate_energy(matrix, j):
-    pass
+
+def calculate_energy(matrix):
+    E = 0
+    matrix_length = len(matrix)
+    for height in range(0, matrix_length):
+        for width in range(0, len(matrix[0])):
+            right = width + 1 if width + 1 < matrix_length else 0
+            down = height + 1 if height + 1 < matrix_length else 0
+            E += J * matrix[width][height] * matrix[right][height]
+            E += J * matrix[width][height] * matrix[width][down]
+    return E
+
 
 matrix_S = init_matrix()
-
+print(calculate_energy(matrix_S))
