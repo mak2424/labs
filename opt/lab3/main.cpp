2a1a088294c8330ccbf0dc809b282b638e5618f4
yoko <dreeborn@mail.ru>
2019-06-16 14:19:26 +1000

    lab3 opt

--- opt/lab3/main.cpp
+++ opt/lab3/main.cpp
@@ -1,9 +1,9 @@
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_matrix.h>
 #include <omp.h>
 #include <stdio.h>
 #include <time.h>
 #include <iostream>
-#include <blas/gsl_blas.h>
-
 
 #define SIZE 1024
 #define BIGSIZE 2048
@@ -16,13 +16,13 @@ double F(double A, double B)
 
 void matmult_native(double* A, double* B, double* C, int size)
 {
-	#pragma omp parallel shared(A,B,C, size)
+	//#pragma omp parallel shared(A,B,C, size)
 	{
 		for (int i = 0; i < size; i++)	
                         for (int k = 0; k < size; k++)
                         {
                                 C[i*size + k] = 0.0;
-                                #pragma omp for nowait
+                                //#pragma omp for nowait
                                 for (int j = 0; j < size; j++)
                                         C[i*size + j] += A[i*size + j] * B[j*size + k];
                         }
@@ -42,12 +42,18 @@ int main()
         {
                 A[i*SIZE+j] = 0.1;
                 B[i*SIZE+j] = 1.1;
+                C[i*SIZE+j] = 0.0;
         }
     printf("  native  matrix multiply ");
-    start = omp_get_wtime();
-    cblas_sgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,M,N,K,1.0,A,K,&B[0][0],N,0.0,C,N);
-    matmult_native(A, B, C, SIZE);
-    stop = omp_get_wtime();
+    start = clock();
+    gsl_matrix_view Ag = gsl_matrix_view_array(A, BIGSIZE, BIGSIZE);
+    gsl_matrix_view Bg = gsl_matrix_view_array(B, BIGSIZE, BIGSIZE);
+    gsl_matrix_view Cg = gsl_matrix_view_array(C, BIGSIZE, BIGSIZE);
+    gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,
+                  1.0, &Ag.matrix, &Bg.matrix,
+                  0.0, &Cg.matrix);
+    //matmult_native(A, B, C, SIZE);
+    stop = clock();
     printf("native matmul  time = %lf seconds\n",((double)(stop - start)) / CLOCKS_PER_SEC);
 
 
